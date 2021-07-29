d926e0bdb9c999d6ee3766db57d1716c90d9b75d
devsecop temp <devsecop.teres@hotmail.com>
2021-07-29 09:09:00 +0000

    Add project files.

---
+++ sonarqube_dotnet_mvc_t1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace sonarqube_dotnet_mvc_t1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ sonarqube_dotnet_mvc_t1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sonarqube_dotnet_mvc_t1.MvcApplication" Language="C#" %>
